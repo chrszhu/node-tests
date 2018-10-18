3aee7f48831237ec7c0afc6d4027857f15f91db0
chris <chrszhu@yahoo.com>
2018-10-17 17:43:26 -0700

    using assertion library expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,24 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`expected 44, got ${res}.`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(5);
-  if (res !== 25) {
-    throw new Error(`expected 25, got ${res}.`);
-  }
-})
+  expect(res).toBe(25).toBeA('number');
+});
+
+it('should verify that first and last name are set', () => {
+  var user = {
+    age: 21,
+    location: "US"
+  };
+  var res = utils.setName(user, "chris zhu");
+  expect(res).toInclude({
+    firstName: 'chris',
+    lastName: 'zhu'
+  });
+});
